a57c2b4f69df93c6833a28d07ea5a0a676777503
Mark Pearyer <marcusp619@gmail.com>
2019-05-02 20:48:13 -0600

    Add data

---
+++ utils/data.js
@@ -0,0 +1,110 @@
+const data = [
+  {
+    title: "CSS-Tricks",
+    url: "https://css-tricks.com/",
+    category: "CSS",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "CSS Grid",
+    url: "https://css-tricks.com/snippets/css/complete-guide-grid/",
+    category: "CSS",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "CSS Flexbox",
+    url: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+    category: "CSS",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "MDN Web Docs",
+    url: "https://developer.mozilla.org/en-US/",
+    category: "General",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Dribbble",
+    url: "https://dribbble.com/",
+    category: "CSS",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Behance",
+    url: "https://www.behance.net/",
+    category: "CSS",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Codepen",
+    url: "https://codepen.io/",
+    category: "CSS",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "You Don't Know JS (book series)",
+    url: "https://github.com/getify/You-Dont-Know-JS",
+    category: "Javascript",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Unsplash",
+    url: "https://unsplash.com",
+    category: "General",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Hacker Rank",
+    url: "https://www.hackerrank.com/",
+    category: "General",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Codewars",
+    url: "https://www.codewars.com/",
+    category: "General",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Exercism",
+    url: "https://exercism.io/",
+    category: "General",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Devchat.tv",
+    url: "https://devchat.tv/",
+    category: "Podcast",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "FreeCodeCamp",
+    url: "https://freecodecamp.libsyn.com/",
+    category: "Podcast",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Syntax",
+    url: "https://syntax.fm/",
+    category: "Podcast",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Sprint UX",
+    url: "https://www.sprintuxpodcast.com/",
+    category: "Podcast",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Developer Tea",
+    url: "https://spec.fm/podcasts/developer-tea",
+    category: "Podcast",
+    added_by: "Mark Pearyer"
+  },
+  {
+    title: "Thoughtbot Podcast",
+    url: "https://thoughtbot.com/podcasts",
+    category: "Podcast",
+    added_by: "Mark Pearyer"
+  }
+];
